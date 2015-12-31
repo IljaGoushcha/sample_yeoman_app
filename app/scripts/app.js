b658e5b0ee728841e909bde65e58227d9d3dcb6f
Ilja Goushcha <iljagou@yahoo.com>
2015-12-31 18:23:22 -0500

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sampleYeomanAppApp
+ * @description
+ * # sampleYeomanAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sampleYeomanAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sampleYeomanAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sampleYeomanAppApp
+ */
+angular.module('sampleYeomanAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
