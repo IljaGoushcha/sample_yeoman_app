2bf0df1b84136d3267313ec4afab876d8c7f11b3
Ilja Goushcha <iljagou@yahoo.com>
2016-01-02 17:07:56 -0500

    working on adding a new course

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,6 +15,7 @@ angular
     'ngResource',
     'ngSanitize',
     'ngTouch',
+    'ui.bootstrap',
     'ui.router',
     'restangular',
     'appModules'
--- app/scripts/courses/controllers/courses-ctrl.js
+++ app/scripts/courses/controllers/courses-ctrl.js
@@ -4,6 +4,21 @@
 	angular.module('coursesModule')
 		.controller('coursesCtrl', ['$scope', 'coursesDataService', function($scope, coursesDataService) {
 
+			$scope.showNewCourseRow = false;
+			$scope.testCollapseVariable = true;
+			$scope.newCourseObject = {
+				"courseName": "",
+				"courseNumber": "",
+				"sectionNumber": ""
+			};
+
+			$scope.addNewCourseAction = function() {
+				console.log("inside addNewCourseAction()");
+				$scope.showNewCourseRow = true;
+				$scope.testCollapseVariable = false;
+				console.log($scope.newCourseObject);
+			};
+
 			$scope.onLoad = function() {
 				console.log("inside onLoad()");
 				coursesDataService.fetchAllCourses().then(function(response) {
