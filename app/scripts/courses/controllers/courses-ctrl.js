244cd15eb890dd8dc070e8000947a327eb90c47c
Ilja Goushcha <iljagou@yahoo.com>
2016-01-03 01:37:15 -0500

    working on CRUD for courses

--- app/scripts/courses/controllers/courses-ctrl.js
+++ app/scripts/courses/controllers/courses-ctrl.js
@@ -3,32 +3,52 @@
 
 	angular.module('coursesModule')
 		.controller('coursesCtrl', ['$scope', 'coursesDataService', function($scope, coursesDataService) {
-
-			$scope.showNewCourseRow = false;
-			$scope.testCollapseVariable = true;
 			
 			$scope.addNewCourseAction = function() {
 				console.log("inside addNewCourseAction()");
-				$scope.showNewCourseRow = true;
-				$scope.testCollapseVariable = false;
+				$scope.allCourses.push({
+					"name": null,
+					"number": null,
+					"section": null,
+					"isSelected": "true"
+				});
+				$scope.unsavedCourseExists = true;
+			};
+
+			$scope.stopAddingNewCourseAction = function() {
+				console.log("inside astoAddingNewCourseAction()");
+				$scope.allCourses.pop();
+				$scope.unsavedCourseExists = false;
 			};
 
 			$scope.saveNewCourseAction = function(newCourse) {
 				console.log("inside saveNewCourseAction()");
 				coursesDataService.saveNewCourse(newCourse).then(function(response) {
 					console.log(response);
+					$scope.onLoad();
 				}, function(error) {
 
 				});
+			};
+
+			$scope.editCourseAction = function(myCourse) {
+				console.log("inside editCourseAction()");
+				myCourse.isSelected = true;
+				$scope.unsavedCourseExists = true;
 			};
 
 			$scope.deleteCourseAction = function(myCourse) {
 				console.log("inside deleteCourseAction()");
 				coursesDataService.deleteCourse(myCourse).then(function(response) {
 					console.log(response);
+					$scope.onLoad();
 				}, function(error) {
 
 				});
+			};
+
+			$scope.showJsonAction = function() {
+				console.log($scope.allCourses);
 			};
 
 			$scope.onLoad = function() {
@@ -36,6 +56,7 @@
 				coursesDataService.fetchAllCourses().then(function(response) {
 					console.log(response);
 					$scope.allCourses = response;
+					$scope.unsavedCourseExists = false;
 				}, function(error) {
 
 				});
